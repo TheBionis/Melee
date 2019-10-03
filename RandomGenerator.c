3cb1e1e5022ce73ce89d28d3f1c7a0298515942a
TheBionis <ronanryan0608@gmail.com>
2019-10-03 14:14:34 -0400

    First attempt at using Git. Melee Random Generator v1.

---
+++ RandomGenerator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NUMBER_OF_STRINGS 26
+#define MAX_STRING_SIZE 20
+
+void print_element(char arr[NUMBER_OF_STRINGS][MAX_STRING_SIZE], int n){
+	for(int j = 0; j < strlen(arr[n]); j++){
+		printf("%c", arr[n][j]);
+	}	
+	printf("\n");
+}
+
+int main(){
+	char arr[NUMBER_OF_STRINGS][MAX_STRING_SIZE] = {"Dr. Mario", "Mario", "Luigi", "Bowser", "Peach", "Yoshi", "DK", "Captain Falcon", "Ganondorf", "Falco", "Fox", "Ness", "Ice Climbers", "Kirby", "Samus", "Zelda", "Sheik", "Link", "Young Link", "Pichu", "Pikachu", "Jigglypuff", "Mewtwo", "Mr. Game & Watch", "Marth", "Roy"};
+
+        int x = rand() % 25;
+        print_element(arr, x);	
+
+}
